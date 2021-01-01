f6a06aff4ffe469120ff9eb8253581697ed8a0cc
y-giryuu <giryuu2@gmail.com>
2021-01-01 16:00:00 +0900

    temp

---
+++ migrations/20201231051259-create-share-record.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('ShareRecords', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      shareName: {
+        type: Sequelize.STRING
+      },
+      favorit: {
+        type: Sequelize.INTEGER
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('ShareRecords');
+  }
+};
